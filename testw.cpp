630d9359376991326cd157a7893a708bf5580955
kladko <13399135+kladkogex@users.noreply.github.com>
2020-06-04 16:52:38 +0300

    SKALE-2678 Added test key generation functions

--- testw.cpp
+++ testw.cpp
@@ -71,6 +71,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "stubclient.h"
 #include "SGXRegistrationServer.h"
 #include "SGXWalletServer.h"
+#include "TestKeyGenerator.h"
 #include "testw.h"
 
 
@@ -80,33 +81,6 @@ using namespace std;
 
 default_random_engine randGen((unsigned int) time(0));
 
-string stringFromFr(libff::alt_bn128_Fr &el) {
-
-    mpz_t t;
-    mpz_init(t);
-    el.as_bigint().to_mpz(t);
-    char arr[mpz_sizeinbase(t, 10) + 2];
-    char *tmp = mpz_get_str(arr, 10, t);
-    mpz_clear(t);
-
-    return string(tmp);
-}
-
-
-string convertDecToHex(string dec, int numBytes = 32) {
-    mpz_t num;
-    mpz_init(num);
-    mpz_set_str(num, dec.c_str(), 10);
-
-    vector<char> tmp(mpz_sizeinbase(num, 16) + 2, 0);
-    char *hex = mpz_get_str(tmp.data(), 16, num);
-
-    string result = hex;
-    int n_zeroes = numBytes * 2 - result.length();
-    result.insert(0, n_zeroes, '0');
-
-    return result;
-}
 
 
 sgx_launch_token_t token = {0};
@@ -134,155 +108,10 @@ shared_ptr <string> encryptTestKey() {
 }
 
 
-vector <libff::alt_bn128_Fr> splitStringToFr(const char *coeffs, const char symbol) {
-    string str(coeffs);
-    string delim;
-    delim.push_back(symbol);
-    vector <libff::alt_bn128_Fr> tokens;
-    size_t prev = 0, pos = 0;
-    do {
-        pos = str.find(delim, prev);
-        if (pos == string::npos) pos = str.length();
-        string token = str.substr(prev, pos - prev);
-        if (!token.empty()) {
-            libff::alt_bn128_Fr coeff(token.c_str());
-            tokens.push_back(coeff);
-        }
-        prev = pos + delim.length();
-    } while (pos < str.length() && prev < str.length());
-
-    return tokens;
-}
-
-vector <string> splitStringTest(const char *coeffs, const char symbol) {
-
-    string str(coeffs);
-    string delim;
-    delim.push_back(symbol);
-    vector <string> g2Strings;
-    size_t prev = 0, pos = 0;
-    do {
-        pos = str.find(delim, prev);
-        if (pos == string::npos) pos = str.length();
-        string token = str.substr(prev, pos - prev);
-        if (!token.empty()) {
-            string coeff(token.c_str());
-            g2Strings.push_back(coeff);
-        }
-        prev = pos + delim.length();
-    } while (pos < str.length() && prev < str.length());
-
-    return g2Strings;
-}
-
-libff::alt_bn128_G2 vectStringToG2(const vector <string> &G2_str_vect) {
-
-    libff::alt_bn128_G2 coeff = libff::alt_bn128_G2::zero();
-    coeff.X.c0 = libff::alt_bn128_Fq(G2_str_vect.at(0).c_str());
-    coeff.X.c1 = libff::alt_bn128_Fq(G2_str_vect.at(1).c_str());
-    coeff.Y.c0 = libff::alt_bn128_Fq(G2_str_vect.at(2).c_str());
-    coeff.Y.c1 = libff::alt_bn128_Fq(G2_str_vect.at(3).c_str());
-    coeff.Z.c0 = libff::alt_bn128_Fq::one();
-    coeff.Z.c1 = libff::alt_bn128_Fq::zero();
-
-    return coeff;
-}
 
 
-void sendRPCRequest() {
-
-    HttpClient client(RPC_ENDPOINT);
-    StubClient c(client, JSONRPC_CLIENT_V2);
 
 
-    int n = 16, t = 16;
-    Json::Value ethKeys[n];
-    Json::Value verifVects[n];
-    Json::Value pubEthKeys;
-    Json::Value secretShares[n];
-    Json::Value pubBLSKeys[n];
-    Json::Value blsSigShares[n];
-    vector <string> pubShares(n);
-    vector <string> polyNames(n);
-
-    int schainID = randGen();
-    int dkgID = randGen();
-    for (uint8_t i = 0; i < n; i++) {
-        ethKeys[i] = c.generateECDSAKey();
-        string polyName =
-                "POLY:SCHAIN_ID:" + to_string(schainID) + ":NODE_ID:" + to_string(i) + ":DKG_ID:" + to_string(dkgID);
-        c.generateDKGPoly(polyName, t);
-        polyNames[i] = polyName;
-        verifVects[i] = c.getVerificationVector(polyName, t, n);
-        REQUIRE(verifVects[i]["status"] == 0);
-
-        pubEthKeys.append(ethKeys[i]["publicKey"]);
-    }
-
-    for (uint8_t i = 0; i < n; i++) {
-        secretShares[i] = c.getSecretShare(polyNames[i], pubEthKeys, t, n);
-        for (uint8_t k = 0; k < t; k++) {
-            for (uint8_t j = 0; j < 4; j++) {
-                string pubShare = verifVects[i]["Verification Vector"][k][j].asString();
-                pubShares[i] += convertDecToHex(pubShare);
-            }
-        }
-    }
-
-
-    int k = 0;
-
-    vector <string> secShares(n);
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
-
-            string secretShare = secretShares[i]["secretShare"].asString().substr(192 * j, 192);
-            secShares[i] += secretShares[j]["secretShare"].asString().substr(192 * i, 192);
-            Json::Value verif = c.dkgVerification(pubShares[i], ethKeys[j]["keyName"].asString(), secretShare, t, n, j);
-
-            k++;
-
-        }
-
-
-    BLSSigShareSet sigShareSet(t, n);
-
-    string hash = SAMPLE_HASH;
-
-    auto hash_arr = make_shared < array < uint8_t,
-    32 >> ();
-    uint64_t binLen;
-    if (!hex2carray(hash.c_str(), &binLen, hash_arr->data())) {
-        throw SGXException(INVALID_HEX, "Invalid hash");
-    }
-
-    map <size_t, shared_ptr<BLSPublicKeyShare>> coeffs_pkeys_map;
-
-
-    for (int i = 0; i < t; i++) {
-        string endName = polyNames[i].substr(4);
-        string blsName = "BLS_KEY" + polyNames[i].substr(4);
-        string secretShare = secretShares[i]["secretShare"].asString();
-
-        c.createBLSPrivateKey(blsName, ethKeys[i]["keyName"].asString(), polyNames[i], secShares[i], t,
-                              n);
-        pubBLSKeys[i] = c.getBLSPublicKeyShare(blsName);
-
-        string hash = SAMPLE_HASH;
-        blsSigShares[i] = c.blsSignMessageHash(blsName, hash, t, n, i + 1);
-        REQUIRE(blsSigShares[i]["status"] == 0);
-
-        shared_ptr <string> sig_share_ptr = make_shared<string>(blsSigShares[i]["signatureShare"].asString());
-        BLSSigShare sig(sig_share_ptr, i + 1, t, n);
-        sigShareSet.addSigShare(make_shared<BLSSigShare>(sig));
-
-
-    }
-
-    shared_ptr <BLSSignature> commonSig = sigShareSet.merge();
-    
-}
 
 
 void destroyEnclave() {
@@ -322,7 +151,6 @@ public:
 
 
 TEST_CASE_METHOD(TestFixture, "ECDSA keygen and signature test", "[ecdsa-key-sig-gen]") {
-
 
     vector<char> errMsg(BUF_LEN, 0);
     int errStatus = 0;
@@ -514,7 +342,7 @@ TEST_CASE_METHOD(TestFixture, "DKG public shares test", "[dkg-pub-shares]") {
     for (u_int64_t i = 0; i < g2Strings.size(); i++) {
         vector <string> coeffStr = splitString(g2Strings.at(i).c_str(), ':');
 
-        pubSharesG2.push_back(vectStringToG2(coeffStr));
+        pubSharesG2.push_back(TestKeyGenerator::vectStringToG2(coeffStr));
     }
 
     vector<char> secret(BUF_LEN, 0);
@@ -525,7 +353,8 @@ TEST_CASE_METHOD(TestFixture, "DKG public shares test", "[dkg-pub-shares]") {
 
     signatures::Dkg dkgObj(t, n);
 
-    vector <libff::alt_bn128_Fr> poly = splitStringToFr(secret.data(), colon);
+    vector <libff::alt_bn128_Fr> poly =
+            TestKeyGenerator::splitStringToFr(secret.data(), colon);
     vector <libff::alt_bn128_G2> pubSharesDkg = dkgObj.VerificationVector(poly);
     for (uint32_t i = 0; i < pubSharesDkg.size(); i++) {
         libff::alt_bn128_G2 el = pubSharesDkg.at(i);
@@ -631,7 +460,7 @@ TEST_CASE_METHOD(TestFixture, "DKG_BLS test", "[dkg-bls]") {
             for (uint8_t j = 0; j < 4; j++) {
                 string pubShare = verifVects[i]["verificationVector"][k][j].asString();
                 REQUIRE(pubShare.length() > 60);
-                pubShares[i] += convertDecToHex(pubShare);
+                pubShares[i] += TestKeyGenerator::convertDecToHex(pubShare);
             }
         }
     }
@@ -871,7 +700,7 @@ TEST_CASE_METHOD(TestFixture, "AES_DKG test", "[aes-dkg]") {
         for (uint8_t k = 0; k < t; k++)
             for (uint8_t j = 0; j < 4; j++) {
                 string pubShare = verifVects[i]["verificationVector"][k][j].asString();
-                pubShares[i] += convertDecToHex(pubShare);
+                pubShares[i] += TestKeyGenerator::convertDecToHex(pubShare);
             }
 
     }
@@ -1001,7 +830,7 @@ TEST_CASE_METHOD(TestFixture, "Many threads ecdsa dkg bls", "[many-threads-crypt
     vector <thread> threads;
     int num_threads = 4;
     for (int i = 0; i < num_threads; i++) {
-        threads.push_back(thread(sendRPCRequest));
+        threads.push_back(thread(TestKeyGenerator::sendRPCRequest));
     }
 
     for (auto &thread : threads) {
